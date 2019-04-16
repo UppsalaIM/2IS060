10b77b3df3949028541a20e6bff8f5c095a1d09f
David Johnson <djcomlab@gmail.com>
2019-04-16 14:37:39 +0100

    Add Lab 3

--- somialabs/setup.py
+++ somialabs/setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/UppsalaIM/2IS060/somialabs',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
